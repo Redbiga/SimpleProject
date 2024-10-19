28eb5d02eb84fc539528a6c612b2c3cee46731de
山麋一笑 <95234477+Redbiga@users.noreply.github.com>
2024-10-20 00:27:18 +0800

    test: Callable和Runnable的区别
    - 新增测试用例 Callable和Runnable的区别
    - 通过实现 Callable接口创建可返回结果的任务- 使用 FutureTask 包装 Callable 任务并执行
    - 展示了 Callable 和 Runnable 的主要区别：
      - Callable 可以返回结果，而 Runnable 不能 - Callable 可以抛出异常，而 Runnable 不能
    -通过异常处理（注释部分）展示了 Callable 的异常处理机制

---
+++ JwtRBAC/src/test/java/org/example/Demo/Callable和Runnable的区别.java
@@ -0,0 +1,31 @@
+package org.example.Demo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+import static org.springframework.test.context.transaction.TestTransaction.start;
+
+/**
+ * @author redA
+ * @时间: 2024年10月18日 下午8:32
+ */
+public class Callable和Runnable的区别 {
+    @Test
+    public void test(){
+    /*    new Thread(()->
+        {
+            System.out.println("hello");
+            Integer i = 2/0;
+        }
+        ).start();*/
+    Callable<Integer> callable = ()->
+    {
+        Integer i = 2/0;
+        return 1;
+    };
+    FutureTask<Integer> futureTask = new FutureTask<>(callable);
+    new Thread(futureTask).start();
+    }
+}
